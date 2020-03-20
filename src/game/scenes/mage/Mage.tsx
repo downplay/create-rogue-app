fc91355b371c7f2932cd5c6ded6b536b4d70d978
Peter Hurst <ph@downplay.co>
2020-03-20 02:25:59 +0000

    mage stuff

---
+++ src/game/scenes/mage/Mage.tsx
@@ -0,0 +1,14 @@
+import { Monster } from "../../meta/Monster";
+// TODO: depending on biome ...
+// wearing shades / swimwear
+// thick furs
+// building a snowman
+
+const maleVariants = ["🧙‍♂️", "🧙🏻‍♂️", "🧙🏼‍♂️", "🧙🏽‍♂️", "🧙🏾‍♂️", "🧙🏿‍♂️"];
+
+const femaleVariants = ["🧙‍♀️", "🧙🏻‍♀️", "🧙🏼‍♀️", "🧙🏽‍♀️", "🧙🏾‍♀️", "🧙🏿‍♀️"];
+
+export const Mage = () => {
+  // TODO: NPC
+  return <Monster></Monster>;
+};
--- src/game/scenes/mage/WanderingMage.tsx
+++ src/game/scenes/mage/WanderingMage.tsx
@@ -1,5 +1,22 @@
 import React, { useRef } from "react";
 import { RoadLayout } from "../../biomes/plains/RoadLayout";
+import { text } from '../../../engine/text/parse';
+import { entity } from '../../../engine/entity';
+import { Mage } from './Mage';
+import { EntityContext } from '../../../engine/useEntitiesState';
+
+// TODO: Figure out a good way to reuse things between text templates
+
+enum Genders {
+    Male = "male",
+    Female = "female"
+}
+
+type WanderingMageState = {
+    gender: Genders,
+    name: string,
+    kind: string
+}
 
 // TODO:
 // Many scenes (e.g. this, tavern) are transferrable to different biomes. RoadLayout would come from the biome tagged as "OutdoorsLayout",
@@ -8,13 +25,24 @@ import { RoadLayout } from "../../biomes/plains/RoadLayout";
 // so it doesn't spawn solid materials on top of wildlife.
 // Something like the mage can adapt with different costumes and inventory. Familiar could be biome-appropriate animal.
 
-export const WanderingMage = () => {
-  const mageRef = useRef();
+export const WanderingMageEncounter = entity(() => {
+  const mageRef = useRef<EntityContext>();
 
-  const state = useStory`(<NULL($gender=genders)>
-By the side of the track you see <A($appearance)> $(wizard=$(gender)Kinds)),
+  const state:WanderingMageState = useStory(()=>{
+    const warningFireball = async (state:WanderingMageState) => {
+        // TODO: Some contortions required here if we want to avoid promises (for savegame support).
+        // Need a ref to a story handle (2nd arg to this callback) that can be resolved when the fireball lands.
+        // On rehydate, we'll need to get back to this point and can call this function again;
+        // and the state that instances the fireball needs to be restored.
+        // Could handle this by using predetermined id (e.g. "WanderingMageWarningFireball")
+        // Also consider state machines (useful also for e.g switching behaviours, see Barkeep)
+        mageRef
+        return "FWOOOOOOM!"
+    }
+  return text`(<NULL($gender=genders)>
+By the side of the track you see <A($appearance)> $(kind=$(gender)Kinds)),
 accompanied by <A($appearance)> $(familiar=$familiars). <TITLE($subjectPronoun)> looks up as
-you approach, cocks $possessivePronoun head on one side, and $observes.
+you approach, cocks $possessivePronoun head on one side, and $observes. The $familiar $familiarVerbs.
 )
 
 appearance:
@@ -41,8 +69,34 @@ enchantress
 familiars:
 cat
 dog
-rabbit
+rat
 bat
+
+familiarVerbs:
+$(kind=$(familiar)Kinds))
+
+catVerbs:
+hisses
+arches its back
+licks its paws
+mews
+
+dogVerbs:
+growls
+barks
+yaps excitedely
+says, "Ruff!"
+looks hungry
+
+ratVerbs:
+squeaks
+scurries under $possessivePronoun robes
+casts its beady eyes towards you
+
+batVerbs:
+chitters
+doesn't make a sound, but somehow you feel that it did
+flaps its leathery wings
 
 subjectPronoun:
 <IS($gender,female)?s>he
@@ -56,7 +110,7 @@ swints sideways at you
 glares in your direction
 puzzledly watches you
 gazes at a point several feet behind you
-hurls a fireball in your direction! ${warningFireball} It narrowly missing you. "Just a warning shot," $subjectPronoun says, $seriously.
+hurls a fireball in your direction!${warningFireball} It narrowly misses you. "Just a warning shot," $subjectPronoun says, $seriously
 
 seriously:
 pointedly
@@ -66,12 +120,17 @@ intimidatingly
 weightily
 gruffly
 `;
+  }
+
+// TODO: changing the name should update the state
+hasName(state.name||state.kind)
 
   return (
     <>
       <RoadLayout />
       <Mage ref={mageRef} />
       <Familiar owner={mageRef} />
+      {children}
     </>
   );
 };
